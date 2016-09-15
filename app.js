6acd0c04a45bbb5bece1b50ffa3d8a051a91f314
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-15 17:55:05 -0500

    working function

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
-//var array = ["a", "b", "c", "d"];
-//var arrTemp = array.slice(0,2);
-//console.log(array.length);
-//console.log(arrTemp);
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  while(arr.length) newArr.push(arr.splice(0,size));
 
-/*
-var arrHold =[];
-for (var i=0; i < 2; i++)
-{
-  for (var j=0; j < 2; j++)
-  {
-    arrHold.push(array);
-  }
-}
-console.log(arrHold);
-*/
-
-/*
-function Create2DArray(rows) {
-  var arr = [];
-
-  for (var i=0;i<rows;i++) {
-     arr[i] = [];
-  }
-
-  return arr;
+  return newArr;
 }
 
-var newArray = Create2DArray(3);
-
-
-var fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
-for (var i=0; i < 2; i++)
-{
-    newArray.push(fruits[i]);
-
-}
-console.log(newArray);
-*/
-
-var arr = [1,2,3,4,5,6,7,8,9];
-
-var newArr = [];
-while(arr.length) newArr.push(arr.splice(0,3));
-
-console.log(newArr);
+// Input data
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
